e218bc8af1555f91ebd79f78049940f07f940b0b
Yami <rajvanshsingh1357@gmail.com>
2026-01-01 19:56:14 +0530

    Update and delete users using database

--- services/users.py
+++ services/users.py
@@ -45,18 +45,37 @@ def get_user_by_id(user_id: int):
         db.close()
 
 def update_user(user_id: int, name: str | None = None, email: str | None = None):
-    for user in users:
-        if user["id"] == user_id:
-            if name is not None:
-                user["name"] = name
-            if email is not None:
-                user["email"] = email
-            return user
-    return None
+    db = SessionLocal()
+    
+    try:
+        user = db.query(User).filter(User.id == user_id).first()
+        if user is None:
+            return None
+
+        if name is not None:
+            user.name = name
+        if email is not None:
+            user.email = email
+
+        db.commit()
+        db.refresh(user)
+        return user
+    
+    finally:
+        db.close()
 
 
 def delete_user(user_id: int):
-    for i, user in enumerate(users):
-        if user["id"] == user_id:
-            return users.pop(i)
-    return None
+    db = SessionLocal()
+    
+    try:
+        user = db.query(User).filter(User.id == user_id).first()
+        if user is None:
+            return None
+
+        db.delete(user)
+        db.commit()
+        return user
+    
+    finally:
+        db.close()
